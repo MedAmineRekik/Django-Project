21346a0f65c6a5bf072ebd0b982a919ccd52fad6
unknown <medamine.rekik20@gmail.com>
2020-08-23 18:49:31 +0200

    use docker to contain it

--- Users/urls.py
+++ Users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/home', views.UserList.as_view(), name='user_list'),
+    path('/', views.UserList.as_view(), name='user_list'),
     path('view/<int:pk>', views.UserDetail.as_view(), name='user_view'),
     path('', views.UserCreate.as_view(), name='user_new'),
     path('edit/<int:pk>', views.UserUpdate.as_view(), name='user_edit'),
